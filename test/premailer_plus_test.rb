36758282221de54bb2b50e327d2cb653229ab2b2
Mike Green <mike.is.green@gmail.com>
2009-05-25 00:52:08 -0400

    removed tracked but missing test file. also realized that remote html files do not work, only local ones work right now

--- test/premailer_plus_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PremailerPlusTest < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
